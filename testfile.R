b0ef65b1533db4521b15a2bb559fb2290229b248
justingoodwater <36508273+justingoodwater@users.noreply.github.com>
2020-12-22 21:51:07 -0800

    clarify comment on model fit

--- testfile.R
+++ testfile.R
@@ -1,6 +1,6 @@
 library(ISLR)
 attach(Weekly)
 
-# fit a logistic regression model using the Weekly data set
+# fit a logistic regression model using the Weekly data set excluding the variables Today and Year
 fit.logit <- glm(Direction ~ ., data = Weekly[, c(2:7, 9)], family = binomial)
 
